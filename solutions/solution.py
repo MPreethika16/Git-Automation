5742f5740fd9999c36cb6726b3756dc3fe971a0d
MPreethika <manepallypreethika@gmail.com>
2025-04-03 12:44:39 +0530

    updated solution files

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,7 +1,6 @@
 from typing import List
-
 class Solution: 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-         # write your code here
+        # write your code here
  
    
