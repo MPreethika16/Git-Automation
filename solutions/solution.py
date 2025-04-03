2921317278aab75d82deb6fc31f27345c335ae68
MPreethika <manepallypreethika@gmail.com>
2025-04-03 12:46:23 +0530

    updated solution files

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,6 +1,8 @@
-from typing import List
-class Solution: 
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # write your code here
- 
-   
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
