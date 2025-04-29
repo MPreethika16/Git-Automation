cf8b240f163ce35f332fe16cd8213c3445dceb0f
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2025-04-29 22:56:38 +0530

    Update Solution.java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new Hashap<>(); 
-        
+         
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             
