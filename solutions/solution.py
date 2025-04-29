0cbd0da3d316b4bdd33cee9c5be906dc338e8b72
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2025-04-29 22:44:58 +0530

    Update solution.py

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,9 +1,9 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         num_to_index = {}
         for i, num in enumerate(nums):
             complement = target - num
             if complement in num_to_index:
                 return [num_to_index[complement], i]
             num_to_index[num] = i
-        return []  # In case no solution is found
+        return []
