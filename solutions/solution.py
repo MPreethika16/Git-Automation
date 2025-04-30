abf803ef75c1092ab99c3b838d19249cb61fbb6f
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2025-04-30 10:13:11 +0530

    Update solution.py

--- solutions/solution.py
+++ solutions/solution.py
@@ -2,11 +2,5 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_to_index = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_to_index:
-                return [num_to_index[complement], i]
-            num_to_index[num] = i
-        return []  # Fallback if no pair found
+      
  
