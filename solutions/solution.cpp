824127da67a311d52b218a7ce0aa6015330afed0
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2025-04-30 10:11:58 +0530

    Update solution.cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -6,17 +6,7 @@ using namespace std;
 class Solution { 
 public: 
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numToIndex;  // map value -> index
-
-        for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];
-            if (numToIndex.find(complement) != numToIndex.end()) {
-                return {numToIndex[complement], i};
-            }
-            numToIndex[nums[i]] = i;
-        }
-
-        return {};  // return empty vector if no solution found
+      
     } 
 };
  
