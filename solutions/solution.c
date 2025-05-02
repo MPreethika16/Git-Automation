97c0f20be33457ef38b66af3ca6725d77008b8f0
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2025-05-02 13:02:10 +0530

    Update solution.c bf

--- solutions/solution.c
+++ solutions/solution.c
@@ -61,4 +61,17 @@ void freeHashMap(HashMap* map) {
 // Function to find two sum indices
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
  //write your code here
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                int* result = (int*)malloc(2 * sizeof(int));
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
 } 
