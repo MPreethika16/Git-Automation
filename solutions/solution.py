593ec1dba03488b7c0a9159162c2563fe32ff5ed
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2025-05-02 13:01:06 +0530

    Update solution.py bf

--- solutions/solution.py
+++ solutions/solution.py
@@ -2,7 +2,11 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+         for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
       
  
  
